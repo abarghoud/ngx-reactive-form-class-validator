b9936017662fe6224126cffbd306b43c9ac86e45
Anas Barghoud <abarghoud@gmail.com>
2020-11-19 21:31:17 +0100

    chore: add jsdoc for methods and class constructors

--- libs/ngx-reactive-form-class-validator/src/lib/class-validator-form-builder.service.ts
+++ libs/ngx-reactive-form-class-validator/src/lib/class-validator-form-builder.service.ts
@@ -7,7 +7,8 @@ import {
   AsyncValidatorFn,
   FormArray,
   FormControl,
-  FormGroup, ValidatorFn
+  FormGroup,
+  ValidatorFn
 } from '@angular/forms';
 
 import { ClassValidatorFormGroup } from './class-validator-form-group';
@@ -23,10 +24,32 @@ const isAbstractControlOptions = (options: AbstractControlOptions | { [key: stri
 
 @Injectable()
 export class ClassValidatorFormBuilderService {
+  /**
+   * @description
+   * Construct a new `FormGroup` instance.
+   *
+   * @param formClassType the `classType` containing `class-validator` decorators to be used to validate form
+   * @param controlsConfig A collection of child controls. The key for each child is the name
+   * under which it is registered.
+   *
+   * @param options Configuration options object for the `FormGroup`. The object can
+   * have two shapes:
+   *
+   * 1) `AbstractControlOptions` object (preferred), which consists of:
+   * * `validators`: A synchronous validator function, or an array of validator functions
+   * * `asyncValidators`: A single async validator or array of async validator functions
+   * * `updateOn`: The event upon which the control should be updated (options: 'change' | 'blur' |
+   * submit')
+   *
+   * 2) Legacy configuration object, which consists of:
+   * * `validator`: A synchronous validator function, or an array of validator functions
+   * * `asyncValidator`: A single async validator or array of async validator functions
+   *
+   */
   public group(
     formClassType: ClassType<any>,
     controlsConfig: { [p: string]: any },
-    options?: AbstractControlOptions | { [p: string]: any } | null,
+    options?: AbstractControlOptions | { [p: string]: any } | null
   ): ClassValidatorFormGroup {
     // Coming from https://github.com/angular/angular/blob/3b0b7d22109c79b4dceb4ae069c3927894cf1bd6/packages/forms/src/form_builder.ts#L59
     const controls = this.reduceControls(controlsConfig);
@@ -48,23 +71,61 @@ export class ClassValidatorFormBuilderService {
       }
     }
 
-    return new ClassValidatorFormGroup(formClassType, controls, {asyncValidators, updateOn, validators});
+    return new ClassValidatorFormGroup(formClassType, controls, { asyncValidators, updateOn, validators });
   }
 
+  /**
+   * Constructs a new `FormArray` from the given array of configurations,
+   * validators and options.
+   *
+   * @param controlsConfig An array of child controls or control configs. Each
+   * child control is given an index when it is registered.
+   *
+   * @param validatorOrOpts A synchronous validator function, or an array of
+   * such functions, or an `AbstractControlOptions` object that contains
+   * validation functions and a validation trigger.
+   *
+   * @param asyncValidator A single async validator or array of async validator
+   * functions.
+   */
   public array(
     controlsConfig: any[],
     validatorOrOpts?: ValidatorFn | ValidatorFn[] | AbstractControlOptions | null,
-    asyncValidator?: AsyncValidatorFn | AsyncValidatorFn[] | null,
+    asyncValidator?: AsyncValidatorFn | AsyncValidatorFn[] | null
   ): FormArray {
     const controls = controlsConfig.map(control => this.createControl(control));
 
     return new ClassValidatorFormArray(controls, validatorOrOpts, asyncValidator);
   }
 
+
+  /**
+   * @description
+   * Construct a new `FormControl` with the given state, validators and options.
+   *
+   * @param formState Initializes the control with an initial state value, or
+   * with an object that contains both a value and a disabled status.
+   *
+   * @param validatorOrOpts A synchronous validator function, or an array of
+   * such functions, or an `AbstractControlOptions` object that contains
+   * validation functions and a validation trigger.
+   *
+   * @param asyncValidator A single async validator or array of async validator
+   * functions.
+   *
+   * @usageNotes
+   *
+   * ### Initialize a control as disabled
+   *
+   * The following example returns a control with an initial value in a disabled state.
+   *
+   * <code-example path="forms/ts/formBuilder/form_builder_example.ts" region="disabled-control">
+   * </code-example>
+   */
   public control(
     formState: any,
     validatorOrOpts?: ValidatorFn | ValidatorFn[] | AbstractControlOptions | null,
-    asyncValidator?: AsyncValidatorFn | AsyncValidatorFn[] | null,
+    asyncValidator?: AsyncValidatorFn | AsyncValidatorFn[] | null
   ): ClassValidatorFormControl {
     return new ClassValidatorFormControl(formState, validatorOrOpts, asyncValidator);
   }
--- libs/ngx-reactive-form-class-validator/src/lib/class-validator-form-control.ts
+++ libs/ngx-reactive-form-class-validator/src/lib/class-validator-form-control.ts
@@ -12,21 +12,45 @@ export class ClassValidatorFormControl extends FormControl {
   private formGroupClassValue: any;
   private name: string;
 
+  /**
+   * Creates a new `ClassValidatorFormControl` instance.
+   *
+   * @param formState Initializes the control with an initial value,
+   * or an object that defines the initial value and disabled state.
+   *
+   * @param validatorOrOpts A synchronous validator function, or an array of
+   * such functions, or an `AbstractControlOptions` object that contains validation functions
+   * and a validation trigger.
+   *
+   * @param asyncValidator A single async validator or array of async validator functions
+   *
+   */
   public constructor(
     formState?: any,
     validatorOrOpts?: ValidatorFn | ValidatorFn[] | AbstractControlOptions | null,
-    asyncValidator?: AsyncValidatorFn | AsyncValidatorFn[] | null,
+    asyncValidator?: AsyncValidatorFn | AsyncValidatorFn[] | null
   ) {
     super(formState, validatorOrOpts, asyncValidator);
 
     this.setValidatorsWithDynamicValidation(validatorOrOpts);
   }
 
+  /**
+   * @internal
+   */
   public setNameAndFormGroupClassValue(name: string, value: any): void {
     this.name = name;
     this.formGroupClassValue = value;
   }
 
+  /**
+   * Sets the synchronous validators that are active on this control as well as resetting the dynamic `class-validator`.  Calling
+   * this overwrites any existing sync validators.
+   *
+   * When you add or remove a validator at run time, you must call
+   * `updateValueAndValidity()` for the new validation to take effect.
+   *
+   */
   public setValidatorsWithDynamicValidation(newValidator: ValidatorFn | ValidatorFn[] | AbstractControlOptions | undefined): void {
     this.setValidators(
       newValidator
